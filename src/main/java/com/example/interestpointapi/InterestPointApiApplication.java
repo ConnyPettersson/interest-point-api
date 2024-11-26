11c8e879d6edb7acbd7c50881e69c130b76cc519
Conny <pedalconny@hotmail.com>
2024-11-26 10:59:51 +0100

    Created Spring Boot Lab 2 project

---
+++ src/main/java/com/example/interestpointapi/InterestPointApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.interestpointapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestPointApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterestPointApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/interestpointapi/InterestPointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interestpointapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestPointApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
