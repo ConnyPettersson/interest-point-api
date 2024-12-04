22d42e4be15ff8b593733e518003eecdfbb8d0cb
Conny <pedalconny@hotmail.com>
2024-12-04 13:54:53 +0100

    Add basic authentication with in-memory user details and BCrypt password encoding in SecurityConfig.

--- src/main/java/com/example/interestpointapi/config/SecurityConfig.java
+++ src/main/java/com/example/interestpointapi/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,11 +22,35 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/categories").permitAll()
+                        .requestMatchers("/categories").hasRole("ADMIN")
+                        .requestMatchers("/places/my-places").hasRole("USER")
+                        .requestMatchers("/places/**").hasAnyRole("USET", "ADMIN")
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+
+            User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("adminPassword"))
+                .roles("ADMIN")
+                .build(),
+            User.builder()
+                    .username("user")
+                    .password(passwordEncoder().encode("userPassword"))
+                    .roles("USER")
+                    .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
